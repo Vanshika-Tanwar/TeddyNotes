b3b9c610961c6517ac01f7a3ac1bfaaae2a0b7dd
VANSHIKA TANWAR <vanshikatanwar04.bpit@gmail.com>
2026-05-17 20:32:32 +0530

    removed extras

--- app/src/main/java/com/example/teddynotes/ui/theme/Color.kt
+++ app/src/main/java/com/example/teddynotes/ui/theme/Color.kt
@@ -7,16 +7,11 @@ val BackgroundGreen = Color(0xFFC8DEC8)
 val NoteBeige = Color(0xFFF4E4C1)
 
 val PrimaryTextBrown = Color(0xFF4A2E1A)
-val SecondaryTextBrown = Color(0xFF653818)
 
 val BearPrimary = Color(0xFF9C5D30)
-val BearDark = Color(0xFF803C0D)
 val BearDeep = Color(0xFF653818)
 
-val ChatBubble = Color(0xFFFCE8D5)
-
 val SoftWhite = Color(0xFFF8FBF9)
-val DividerLight = Color(0xFFE0E0E0)
 
 val MoodHappy = Color(0xFFFFD166)
 val MoodCalm = Color(0xFFA8D5BA)
